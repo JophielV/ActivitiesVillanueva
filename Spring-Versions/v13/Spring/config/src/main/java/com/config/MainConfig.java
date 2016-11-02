8ae8b9d58b4c74db2a8eb0d2da11e3188294caad
JophielV <jophielvillanueva@gmail.com>
2016-11-02 15:15:13 +0800

    2nd Version to be submitted

---
+++ Spring-Versions/v13/Spring/config/src/main/java/com/config/MainConfig.java
@@ -0,0 +1,80 @@
+package com.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import java.util.Properties;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.service.BusinessLogicService;
+import com.dao.implementations.PersonDAO;
+import com.dao.implementations.ContactInfoDAO;
+import com.dao.interfaces.PersonDaoInterface;
+import com.dao.interfaces.ContactInfoDaoInterface;
+
+
+@Configuration
+@ComponentScan({"com.controller", "com.dao.implementations", "com.service"})
+@Import({ SecurityConfig.class })
+@EnableTransactionManagement
+public class MainConfig {
+
+   String dbUser = "";
+   String dbPassword = "";
+
+   @Bean(name = "viewResolver")
+   public InternalResourceViewResolver getViewResolver() {
+     InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+     viewResolver.setPrefix("/WEB-INF/views/");
+     viewResolver.setSuffix(".jsp");
+     return viewResolver;
+   }
+
+   @Autowired
+   @Bean(name = "sessionFactory")
+   public SessionFactory getSessionFactory(DataSource dataSource) {
+      LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+      sessionBuilder.scanPackages("com.model")
+                          .addProperties(getHibernateProperties());
+      return sessionBuilder.buildSessionFactory();
+   }
+
+   @Bean(name = "dataSource")
+   public DataSource getDataSource() {
+      BasicDataSource dataSource = new BasicDataSource();
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql://localhost:3306/HibernatePart1");
+      dataSource.setUsername("root");
+      dataSource.setPassword("sample");
+ 
+      return dataSource;
+   }
+
+   private Properties getHibernateProperties() {
+      Properties properties = new Properties();
+      properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+      properties.put("hibernate.hbm2ddl.auto", "update");
+      return properties;
+   }
+
+   @Autowired
+   @Bean(name = "transactionManager")
+   public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+      HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+ 
+      return transactionManager;
+   }
+   
+}
---
+++ Spring-Versions/v13/Spring/dao/src/main/java/com/dao/implementations/PersonRolesDAO.java
@@ -0,0 +1,53 @@
+package com.dao.implementations;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.model.Role;
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.Query;
+import java.util.Iterator; 
+import java.util.*; 
+import com.model.Person;
+import com.model.PersonRoles;
+import com.model.Address;
+import com.model.ContactInfo;
+import com.dao.interfaces.PersonRolesDaoInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class PersonRolesDAO implements PersonRolesDaoInterface  {
+
+    @Autowired
+    private SessionFactory sessionFactory;  
+
+    public PersonRolesDAO() {
+         
+    }
+     
+    public PersonRolesDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public List listPersonRoles() {
+        List<PersonRoles> personRoles = sessionFactory.getCurrentSession().createQuery("FROM PersonRoles").list(); 
+          
+        return personRoles;
+    }
+
+    @Transactional
+    public List getRolesAssociatedWithPerson(Integer id) {       
+        Criteria cr = sessionFactory.getCurrentSession().createCriteria(PersonRoles.class, "personRoles")
+        .add(Restrictions.eq("personRoles.personRole.personId", id));
+        List<PersonRoles> personWithRoles = cr.list(); 
+       
+        return personWithRoles;
+    }
+
+}
